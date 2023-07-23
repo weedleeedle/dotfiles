33e0357199f6d416435fb5e90b43caa01114f8aa
ViperZer0 <viperzer0thehackingreptile@gmail.com>
2023-07-22 19:17:26 -0700

    Don't remember what i added here other than more neovim configuration stuff

--- nvim/lua/plugin-setup.lua
+++ nvim/lua/plugin-setup.lua
@@ -1,5 +1,10 @@
-require("telescope").setup { }
+require("telescope").setup {
+    extensions = {
+        picker_list = {},
+    }
+}
 require("telescope").load_extension "file_browser"
+require("telescope").load_extension "picker_list"
 
 local cmp = require('cmp')
 cmp.setup({
@@ -13,14 +18,18 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "vsnip" },
         { name = 'buffer' },
         { name = 'async_path' },
-    })
+    }),
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    }
 })
 
 
