245f5581dc5a7652df8f0e9e41ff24e4d0039280
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2024-01-04 13:58:18 -0800

    Added install.sh, removed config for root stuff (xorg)

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Apply all symbolic links to where they're supposed to go
+SOURCE_DIR=$(pwd)
+ln -sv $SOURCE_DIR/autorandr ~/.config
+ln -sv $SOURCE_DIR/awesome ~/.config
+ln -sv $SOURCE_DIR/cmus ~/.config
+ln -sv $SOURCE_DIR/nvim ~/.config
+ln -sv $SOURCE_DIR/rofi ~/.config
+ln -sv $SOURCE_DIR/.vimrc ~/.vimrc
+ln -sv $SOURCE_DIR/.bash_alias ~/.bash_alias
+ln -sv $SOURCE_DIR/.bashrc ~/.bashrc
+ln -sv $SOURCE_DIR/.gitconfig ~/.gitconfig
+ln -sv $SOURCE_DIR/.gtkrc ~/.gtkrc
+ln -sv $SOURCE_DIR/.XCompose ~/.XCompose
+ln -sv $SOURCE_DIR/.xinitrc ~/.xinitrc
+
