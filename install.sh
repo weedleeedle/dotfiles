cc750262c6be64ec430901d2bc7c4a0348caca2d
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2024-01-05 10:55:18 -0800

    Reverted rofi symlink.
    Rofi uses a symlink on startx to set config.rasi to current theme.

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ ln -sv $SOURCE_DIR/autorandr ~/.config
 ln -sv $SOURCE_DIR/awesome ~/.config
 ln -sv $SOURCE_DIR/cmus ~/.config
 ln -sv $SOURCE_DIR/nvim ~/.config
-ln -sv $SOURCE_DIR/rofi ~/.config
 ln -sv $SOURCE_DIR/.vimrc ~/.vimrc
 ln -sv $SOURCE_DIR/.bash_alias ~/.bash_alias
 ln -sv $SOURCE_DIR/.bashrc ~/.bashrc
@@ -14,3 +13,5 @@ ln -sv $SOURCE_DIR/.gtkrc ~/.gtkrc
 ln -sv $SOURCE_DIR/.XCompose ~/.XCompose
 ln -sv $SOURCE_DIR/.xinitrc ~/.xinitrc
 
+# Make dir for rofi theme
+mkdir ~/.config/rofi
