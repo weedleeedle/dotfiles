a018339ca2162b146aed6ed2475286be1c1016c8
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2024-02-25 17:11:22 -0800

    More keybinds to take advantage of treesitter/LSP stuff

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -15,3 +15,36 @@ vim.keymap.set('n', '<leader>qf', vim.lsp.buf.code_action, {});
 --Line numbers
 vim.keymap.set('n', '<F2>', vim.fn.ToggleLineNumbers, {});
 
+-- LspAttach autocommand only adds maps after language server
+-- attaches to current buffer.
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Buffer mappings
+        local opts = { buffer = ev.buf }
+        -- jump to definition
+        vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
+        -- hover over documentation
+        vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, opts)
+        -- List implementations
+        vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, opts)
+        -- Get signature help
+        vim.keymap.set('n', '<leader><C-K>', vim.lsp.buf.signature_help, opts)
+        -- Jump to type definition
+        vim.keymap.set('n', '<leader>gD', vim.lsp.buf.type_definition, opts)
+        -- Rename symbol
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        -- Code actions
+        vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
+        -- get references
+        vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
+        -- format
+        vim.keymap.set('n', '<leader><space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
+})
+
+
+
+
