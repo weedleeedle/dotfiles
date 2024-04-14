7e54eb11408f7fea0ad8b248e08dcf336092eea9
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2024-04-14 14:27:51 -0700

    Fixed broken symlink generation in installation script

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Apply all symbolic links to where they're supposed to go
 SOURCE_DIR=$(pwd)
-ln -sv $SOURCE_DIR/autorandr ~/.config
-ln -sv $SOURCE_DIR/awesome ~/.config
-ln -sv $SOURCE_DIR/cmus ~/.config
-ln -sv $SOURCE_DIR/nvim ~/.config
+CONFIG_DIR=$HOME/.config
+mkdir -p $CONFIG_DIR
+
+ln -sv $SOURCE_DIR/awesome $CONFIG_DIR/awesome
+ln -sv $SOURCE_DIR/cmus $CONFIG_DIR
+ln -sv $SOURCE_DIR/nvim $CONFIG_DIR
 ln -sv $SOURCE_DIR/.vimrc ~/.vimrc
 ln -sv $SOURCE_DIR/.bash_alias ~/.bash_alias
 ln -sv $SOURCE_DIR/.bashrc ~/.bashrc
@@ -15,4 +17,4 @@ ln -sv $SOURCE_DIR/.xinitrc ~/.xinitrc
 ln -sv $SOURCE_DIR/.scripts ~/.scripts
 
 # Make dir for rofi theme
-mkdir ~/.config/rofi
+mkdir $CONFIG_DIR/rofi
