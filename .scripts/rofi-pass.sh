cd3675780dcc0dd3171e46111cf2c52dc0f50d55
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2024-04-14 15:26:54 -0700

    Reworked rofi-pass to show all passwords, including those in subfolders

--- .scripts/rofi-pass.sh
+++ .scripts/rofi-pass.sh
@@ -4,28 +4,14 @@
 # including those in subdirectories. When a password is chosen it is copied to
 # the clipboard. Shouldn't really be called by hand?
 PASS_DIR=$HOME/.password-store/
-# We need an external file to keep track of what directory we are "in"
-STATE_FILE=/tmp/rofi_current_dir
 
-# Reset state file if on a brand new invocation (i.e no arguments)
-if [ $# -eq 0 ]; then
-    echo "" > "$STATE_FILE"
-fi
-
-CURRENT_DIR=$(<$STATE_FILE)
-# If the supplied argument is the password file, execute and return nothing.
-if [ -f "${PASS_DIR}${CURRENT_DIR}$1.gpg" ] ; then
-    nohup sh -c "clipctl disable && /usr/bin/pass show -c ${CURRENT_DIR}$1 ; clipctl enable" > /dev/null 2>&1  &
-    # Reset state file
-    echo "" > "$STATE_FILE"
+# If there is an argument, execute password retrieval and return nothing.
+if [ $# -eq 1 ] ; then
+    nohup sh -c "clipctl disable && /usr/bin/pass show -c $1 ; clipctl enable" > /dev/null 2>&1  &
     exit 0
 fi
 
-# If we enter a directory, update the state file
-if [ -d "${PASS_DIR}${CURENT_DIR}$1" ] ; then
-    echo "${CURRENT_DIR}$1/" > "$STATE_FILE"
+# If there is no argument, list all the available passwords
+if [ $# -eq 0 ] ; then
+    find $PASS_DIR -name "*.gpg" -printf "%P\n" | sed "s/.gpg$//"
 fi
-# Otherwise we "auto complete"
-results=$(/usr/bin/ls -1 $PASS_DIR$CURRENT_DIR$1 | sed "s/\.gpg$//")
-echo "$results"
-
