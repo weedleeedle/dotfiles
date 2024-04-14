496d663e3fae7d4121e6b22de18e4df9c5131400
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2024-04-14 14:22:41 -0700

    Added packer bootstrap to automate installation on new machines

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,17 @@
+-- Bootstrap Packer
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
@@ -83,6 +97,12 @@ return require('packer').startup(function(use)
             "rcarriga/nvim-notify",
         }
     }
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- (bootstrap)
+    -- KEEP THIS AT THE END
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
 
 
