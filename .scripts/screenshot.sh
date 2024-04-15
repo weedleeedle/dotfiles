cf5199e0867c1cbf418eca4e7b252c7ce4e50cd9
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2024-04-14 17:54:53 -0700

    Fixed scrot bug, now properly calls escrotum.
    While I had an alias for scrot's replacement (escrotum), apparently the
    script was not using the alias so it was still using scrot. On my new
    machine I don't have scrot, so it was failing.

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -5,10 +5,10 @@
 # prompted to select an area to screenshot.
 if [ "$1" == -g ]
 then
-	scrot -e 'mv $f /tmp/screenshot.png' 2>scroterror
+	escrotum -e 'mv $f /tmp/screenshot.png' 2>scroterror && 
 	xclip -selection clipboard -target image/png -i /tmp/screenshot.png
 else
 	sleep 1
-	scrot -s -e 'mv $f /tmp/screenshot.png' 2>scroterror
+	escrotum -s -e 'mv $f /tmp/screenshot.png' 2>scroterror &&
 	xclip -selection clipboard -target image/png -i /tmp/screenshot.png
 fi
