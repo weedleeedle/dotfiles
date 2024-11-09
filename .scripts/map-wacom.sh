878a699bde1c473ee65f5424a497f13345d63b5d
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2024-11-09 00:23:02 -0800

    New wacom tablet, fixed wacom script (ish)

--- .scripts/map-wacom.sh
+++ .scripts/map-wacom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 1
-ID=`xsetwacom list devices | grep "STYLUS" | awk '{print $7}'`
+ID=`xsetwacom list devices | grep "STYLUS" | awk '{print $9}'`
 echo $ID
 xsetwacom set $ID MapToOutput "HEAD-0"
