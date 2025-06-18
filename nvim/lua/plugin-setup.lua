981fca99ae103f636dfbac49340665faa9524590
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-06-17 23:47:05 -0700

    Changed autocomplete bindings.

--- nvim/lua/plugin-setup.lua
+++ nvim/lua/plugin-setup.lua
@@ -42,10 +42,10 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
     }),
+
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "vsnip" },
