55cda19d56ebc719d2d7e141a0b3e8cb6b2a3a45
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-10-19 16:46:13 -0700

    Added Godot debug stuff to NeoVim

--- nvim/lua/plugin-setup.lua
+++ nvim/lua/plugin-setup.lua
@@ -55,8 +55,32 @@ cmp.setup({
     window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
-    }
+    },
+    completion = {}
 })
+
+local dap = require("dap")
+dap.adapters.godot = {
+    type = "server",
+    host = "127.0.0.1",
+    port = 6006,
+}
+
+dap.configurations.gdscript = {
+    {
+        type = "godot",
+        request = "launch",
+        name = "Launch scene",
+        project = "${workspaceFolder}",
+        launch_scene = true
+    },
+}
+
+vim.api.nvim_create_user_command("Breakpoint", "lua require'dap'.toggle_breakpoint()", {})
+vim.api.nvim_create_user_command("Continue", "lua require'dap'.continue()", {})
+vim.api.nvim_create_user_command("StepOver", "lua require'dap'.step_over()", {})
+vim.api.nvim_create_user_command("StepInto", "lua require'dap'.step_into()", {})
+vim.api.nvim_create_user_command("REPL", "lua require'dap'.repl.open()", {})
 
 --[[
 require("noice").setup({
